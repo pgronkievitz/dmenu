9d386a2003cf3b6c7b0c85279d7b8d737301996b
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-04-07 21:23:30 +0200

    init

---
+++ config.h
@@ -0,0 +1,27 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=10"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+                   /*   fg          bg    */
+    [SchemeNorm] = { "#F8F8F2", "#282A36" },
+    [SchemeSel]  = { "#282A36", "#BD93F9" },
+    [SchemeOut]  = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 20;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+/* Size of the window border */
+static const unsigned int border_width = 2;
