04e08ee7e661a3088e76d767a6c532f15dbe974a
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-04-08 10:14:00 +0200

    remove fuzzymatch due to case sensitivity

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetbrainsMono Nerd Font Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -25,4 +25,4 @@ static unsigned int lines      = 20;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 1;
--- dmenu.c
+++ dmenu.c
@@ -32,7 +32,6 @@ struct item {
 	char *text;
 	struct item *left, *right;
 	int out;
-	int distance;
 };
 
 static char text[BUFSIZ] = "";
@@ -156,7 +155,7 @@ drawmenu(void)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	if (passwd) {
 	        censort = ecalloc(1, sizeof(text));
-		memset(censort, '.', strlen(text));
+		memset(censort, '*', strlen(text));
 		drw_text(drw, x, 0, w, bh, lrpad / 2, censort, 0);
 		free(censort);
 	} else drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
@@ -280,84 +279,6 @@ match(void)
 	calcoffsets();
 }
 
-static int
-compare_distance(const void *a, const void *b)
-{
-	struct item const *da = *(struct item **) a;
-	struct item const *db = *(struct item **) b;
-
-	if (!db)
-		return 1;
-	if (!da)
-		return -1;
-	return da->distance - db->distance;
-}
-
-static void
-fuzzymatch(void)
-{
-	struct item *item;
-	struct item **fuzzymatches = NULL;
-	char c;
-	int number_of_matches = 0, i, pidx, sidx, eidx;
-	int text_len = strlen(text), itext_len;
-
-	matches = matchend = NULL;
-
-	/* walk through all items */
-	for (item = items; item && item->text; item++) {
-		if (text_len) {
-			itext_len = strlen(item->text);
-			pidx = 0;
-			sidx = eidx = -1;
-			/* walk through item text */
-			for (i = 0; i < itext_len && (c = item->text[i]); i++) {
-				/* fuzzy match pattern */
-				if (text[pidx] == c) {
-					if (sidx == -1)
-						sidx = i;
-					pidx++;
-					if (pidx == text_len) {
-						eidx = i;
-						break;
-					}
-				}
-			}
-			/* build list of matches */
-			if (eidx != -1) {
-				/* compute distance */
-				/* factor in 30% of sidx and distance between eidx and total
-				 * text length .. let's see how it works */
-				item->distance = eidx - sidx + (itext_len - eidx + sidx) / 3;
-				appenditem(item, &matches, &matchend);
-				number_of_matches++;
-			}
-		}
-		else
-			appenditem(item, &matches, &matchend);
-	}
-
-	if (number_of_matches) {
-		/* initialize array with matches */
-		if (!(fuzzymatches = realloc(fuzzymatches, number_of_matches * sizeof(struct item*))))
-			die("cannot realloc %u bytes:", number_of_matches * sizeof(struct item *));
-		for (i = 0, item = matches; item && i < number_of_matches; i++, item = item->right)
-			fuzzymatches[i] = item;
-
-		/* sort matches according to distance */
-		qsort(fuzzymatches, number_of_matches, sizeof(struct item *), compare_distance);
-		/* rebuild list of matches */
-		matches = matchend = NULL;
-		for (i = 0, item = fuzzymatches[0]; i < number_of_matches && item && \
-				item->text; item = fuzzymatches[i], i++)
-			appenditem(item, &matches, &matchend);
-
-		free(fuzzymatches);
-	}
-	curr = sel = matches;
-	calcoffsets();
-}
-
 static void
 insert(const char *str, ssize_t n)
 {
@@ -368,7 +289,7 @@ insert(const char *str, ssize_t n)
 	if (n > 0)
 		memcpy(&text[cursor], str, n);
 	cursor += n;
-	fuzzymatch();
+	match();
 }
 
 static size_t
@@ -437,7 +358,7 @@ keypress(XKeyEvent *ev)
 
 		case XK_k: /* delete right */
 			text[cursor] = '\0';
-			fuzzymatch();
+			match();
 			break;
 		case XK_u: /* delete left */
 			insert(NULL, 0 - cursor);
@@ -586,7 +507,7 @@ insert:
 		strncpy(text, sel->text, sizeof text - 1);
 		text[sizeof text - 1] = '\0';
 		cursor = strlen(text);
-		fuzzymatch();
+		match();
 		break;
 	}
 
@@ -766,7 +687,7 @@ setup(void)
 		}
 	}
 	inputw = MIN(inputw, mw/3);
-	fuzzymatch();
+	match();
 
 	/* create menu window */
 	swa.override_redirect = True;
